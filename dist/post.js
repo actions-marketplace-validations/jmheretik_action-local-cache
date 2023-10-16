465ce9baf4e1af84704fba1fe92caed566382744
Jakub Medvecký-Heretik <5638498+jmheretik@users.noreply.github.com>
2023-10-16 10:23:57 +0200

    Update post.js

--- dist/post.js
+++ dist/post.js
@@ -2866,7 +2866,7 @@ var import_io = __toESM(require_io());
 
 // src/lib/getVars.ts
 var core = __toESM(require_core());
-var { GITHUB_REPOSITORY, RUNNER_TOOL_CACHE, CUSTOM_RUNNER_TOOL_CACHE } = process.env;
+var { GITHUB_REPOSITORY, RUNNER_TOOL_CACHE } = process.env;
 var CWD = process.cwd();
 var getVars = () => {
   if (!RUNNER_TOOL_CACHE) {
@@ -2877,12 +2877,13 @@ var getVars = () => {
   }
   const options = {
     key: core.getInput("key") || "no-key",
-    path: core.getInput("path")
+    path: core.getInput("path"),
+    dir: core.getInput("dir")
   };
   if (!options.path) {
     throw new TypeError("path is required but was not provided.");
   }
-  const cacheDir = path__default.default.join(CUSTOM_RUNNER_TOOL_CACHE ?? RUNNER_TOOL_CACHE, GITHUB_REPOSITORY, options.key);
+  const cacheDir = options.dir ? path__default.default.join(options.dir, options.key) : path__default.default.join(RUNNER_TOOL_CACHE, GITHUB_REPOSITORY, options.key);
   const cachePath = path__default.default.join(cacheDir, options.path);
   const targetPath = path__default.default.resolve(CWD, options.path);
   const { dir: targetDir } = path__default.default.parse(targetPath);
